c829dc429d217000677d2889173d10b835a18a2f
Sam Severance <sam.severance@merrillcorp.com>
2020-07-10 12:37:39 -0400

    add bugfix example

--- src/components-examples/material/datepicker/date-range-picker-forms/date-range-picker-forms-example.ts
+++ src/components-examples/material/datepicker/date-range-picker-forms/date-range-picker-forms-example.ts
@@ -1,15 +1,67 @@
 import {Component} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
 
-/** @title Date range picker forms integration */
+const dateOffset = (24*60*60*1000) * 5; //5 days
+
+/** @title Date range picker bugfix */
 @Component({
   selector: 'date-range-picker-forms-example',
-  templateUrl: 'date-range-picker-forms-example.html',
-  styleUrls: ['date-range-picker-forms-example.css'],
+  template: `
+        <mat-form-field>
+            <mat-label>Works</mat-label>
+            <mat-date-range-input [formGroup]="range" [rangePicker]="picker" [min]="min" [max]="max">
+                <input matStartDate formControlName="start"
+                       placeholder="Start date">
+                <input matEndDate formControlName="end"
+                       placeholder="End date">
+            </mat-date-range-input>
+            <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
+            <mat-date-range-picker #picker></mat-date-range-picker>
+        </mat-form-field>
+
+        <mat-form-field>
+            <mat-label>Doesn't work</mat-label>
+            <mat-date-range-input [formGroup]="range2" [rangePicker]="picker2" [min]="getMin()" [max]="getMax()">
+                <input matStartDate formControlName="start"
+                       placeholder="Start date">
+                <input matEndDate formControlName="end"
+                       placeholder="End date">
+            </mat-date-range-input>
+            <mat-datepicker-toggle matSuffix [for]="picker2"></mat-datepicker-toggle>
+            <mat-date-range-picker #picker2></mat-date-range-picker>
+        </mat-form-field>
+  `
 })
 export class DateRangePickerFormsExample {
+
+  now = new Date();
+  min: Date;
+  max: Date;
+
   range = new FormGroup({
-    start: new FormControl(),
-    end: new FormControl()
+      start: new FormControl(),
+      end: new FormControl()
   });
+
+  range2 = new FormGroup({
+      start: new FormControl(),
+      end: new FormControl()
+  });
+
+  constructor() {
+      this.min = this.getMin();
+      this.max = this.getMax();
+  }
+
+  getMin() {
+      let min = new Date();
+      min.setTime(this.now.getTime() - dateOffset);
+      return min;
+  }
+
+  getMax() {
+      let max = new Date();
+      max.setTime(this.now.getTime() + dateOffset);
+      return max;
+  }
 }
